4e6d3b60a6043af99a4e87d5ee8f83d3b6128469
imaimai17468 <tosiki_801_life@outlook.jp>
2025-07-20 02:03:43 +0900

    feat: アプリのアイコンを追加し、ページにアニメーション効果を適用しました。また、メタデータを更新しました。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
-import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="flex h-screen flex-col items-center justify-center">
-			<h1 className="font-bold text-4xl">Hello World</h1>
-			<p className="text-lg">This is a test</p>
-			<Button>Click me</Button>
+		<div>
+			<div className="space-y-2">
+				<Image
+					src="/app-icon.png"
+					alt="logo"
+					width={150}
+					height={150}
+					className="mx-auto opacity-50"
+				/>
+				<h1 className="text-center font-bold text-4xl">
+					Code less. Decide more.
+				</h1>
+				<div className="animate-fade-in text-center">
+					<p className="max-w-xl text-gray-400 text-sm">
+						The platform for those who write the rules behind the AI.
+					</p>
+					<p className="max-w-xl text-gray-400 text-sm">
+						Share project-ready rule templates for Claude, Cursor, and more
+					</p>
+				</div>
+			</div>
 		</div>
 	);
 }
